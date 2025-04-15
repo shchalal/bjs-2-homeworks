19c51d945b4c7554df5ff3a46973176d203c705b
Svetlana Shcherbakova <shchalal@mail.ru>
2025-04-15 17:43:25 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,19 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let discriminant = b ** 2 - 4 * a * c;
+
+	if (discriminant < 0) {
+		return [];
+	} else if (discriminant === 0) {
+		let root = -b / (2 * a);
+		return [root];
+	} else {
+		let sqrtDiscriminant = Math.sqrt(discriminant);
+		let root1 = (-b + sqrtDiscriminant) / (2 * a);
+		let root2 = (-b - sqrtDiscriminant) / (2 * a);
+		return [root1, root2];
+	}
+}
 }
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}
